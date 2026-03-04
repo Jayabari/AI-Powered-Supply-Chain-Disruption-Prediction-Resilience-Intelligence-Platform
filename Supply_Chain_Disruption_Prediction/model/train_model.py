aeb6ad1026077627f1aa5a9c94162c04054ec443
Jaya <jayabari849@gmail.com>
2026-03-04 20:49:17 +0530

    Update train_model.py

--- Supply_Chain_Disruption_Prediction/model/train_model.py
+++ Supply_Chain_Disruption_Prediction/model/train_model.py
@@ -7,12 +7,10 @@ from sklearn.preprocessing import LabelEncoder
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score, classification_report
 
-# Load data
+
 df = pd.read_csv("../data/supply_chain.csv")
 
-# -----------------------------
-# CREATE DISRUPTION LABEL
-# -----------------------------
+
 df['disruption'] = np.where(
     (df['Availability'] < 20) |
     (df['Stock levels'] < 20) |
@@ -22,18 +20,14 @@ df['disruption'] = np.where(
     1, 0
 )
 
-# -----------------------------
-# DROP NON-USEFUL COLUMNS
-# -----------------------------
+
 df.drop([
     'SKU',
     'Customer demographics',
     'Inspection results'
 ], axis=1, inplace=True)
 
-# -----------------------------
-# ENCODE CATEGORICAL DATA
-# -----------------------------
+
 categorical_cols = [
     'Product type',
     'Shipping carriers',
@@ -47,22 +41,16 @@ encoder = LabelEncoder()
 for col in categorical_cols:
     df[col] = encoder.fit_transform(df[col])
 
-# -----------------------------
-# FEATURES & TARGET
-# -----------------------------
+
 X = df.drop('disruption', axis=1)
 y = df['disruption']
 
-# -----------------------------
-# TRAIN TEST SPLIT
-# -----------------------------
+
 X_train, X_test, y_train, y_test = train_test_split(
     X, y, test_size=0.2, random_state=42
 )
 
-# -----------------------------
-# MODEL (HIGH ACCURACY)
-# -----------------------------
+
 model = RandomForestClassifier(
     n_estimators=300,
     max_depth=10,
@@ -71,16 +59,12 @@ model = RandomForestClassifier(
 
 model.fit(X_train, y_train)
 
-# -----------------------------
-# EVALUATION
-# -----------------------------
+
 y_pred = model.predict(X_test)
 accuracy = accuracy_score(y_test, y_pred)
 
 print("Accuracy:", accuracy)
 print(classification_report(y_test, y_pred))
 
-# -----------------------------
-# SAVE MODEL
-# -----------------------------
+
 joblib.dump(model, "supply_chain_model.pkl")
