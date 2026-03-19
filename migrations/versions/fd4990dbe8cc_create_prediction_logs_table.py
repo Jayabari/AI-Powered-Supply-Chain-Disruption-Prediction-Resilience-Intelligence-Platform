381da1985c569c3779fe822b26d49424c43aa499
Rishabh Barnwal <rishabhbarnwal1912@gmail.com>
2026-03-19 13:31:01 +0530

    feat: Create migration for prediction_logs table

---
+++ migrations/versions/fd4990dbe8cc_create_prediction_logs_table.py
@@ -0,0 +1,24 @@
+"""create prediction_logs table
+
+Revision ID: fd4990dbe8cc
+Revises: 
+Create Date: 2026-03-19 13:30:55.690552
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'fd4990dbe8cc'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
