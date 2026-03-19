a143dabf25fdf4128c29f5d77bdf6c405b0ebeab
Rishabh Barnwal <rishabhbarnwal1912@gmail.com>
2026-03-19 13:32:02 +0530

    feat: Implement creation and deletion of prediction_logs table in migration

--- migrations/versions/fd4990dbe8cc_create_prediction_logs_table.py
+++ migrations/versions/fd4990dbe8cc_create_prediction_logs_table.py
@@ -17,8 +17,22 @@ depends_on = None
 
 
 def upgrade():
-    pass
+    op.create_table(
+        'prediction_logs',
+        sa.Column('id', sa.Integer(), nullable=False),
+        sa.Column('event_type', sa.String(length=100), nullable=False),
+        sa.Column('severity_level', sa.String(length=20), nullable=False),
+        sa.Column('cause', sa.String(length=120), nullable=False),
+        sa.Column('country', sa.String(length=120), nullable=False),
+        sa.Column('financial_impact', sa.Float(), nullable=False),
+        sa.Column('prediction', sa.Integer(), nullable=False),
+        sa.Column('probability', sa.Float(), nullable=False),
+        sa.Column('risk_level', sa.String(length=20), nullable=False),
+        sa.Column('source', sa.String(length=20), nullable=False),
+        sa.Column('created_at', sa.DateTime(timezone=True), nullable=False),
+        sa.PrimaryKeyConstraint('id'),
+    )
 
 
 def downgrade():
-    pass
+    op.drop_table('prediction_logs')
