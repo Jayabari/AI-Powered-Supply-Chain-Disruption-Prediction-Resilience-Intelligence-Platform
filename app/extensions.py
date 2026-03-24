a937799418016fee05c0780efdf6b4f1d3f55b72
ananshi <ananshi.gla_cs.aiml24@gla.ac.in>
2026-03-24 11:02:25 +0530

    refactor: reorganize import statements in extensions.py

--- app/extensions.py
+++ app/extensions.py
@@ -4,6 +4,9 @@ from flask_limiter import Limiter
 from flask_limiter.util import get_remote_address
 from flask_login import LoginManager
 from flask_migrate import Migrate
+from flask_sqlalchemy import SQLAlchemy
+from flask_talisman import Talisman
+from flask_wtf.csrf import CSRFProtect
 
 
 db = SQLAlchemy()
